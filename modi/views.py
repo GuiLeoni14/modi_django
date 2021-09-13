f1f8a1fa6f4c71010a33f36fd3c0583f4d36e671
GuiLeoni14 <guileone167@gmail.com>
2021-09-13 15:39:12 -0300

    ajustando template

--- modi/views.py
+++ modi/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+#from modi.models import modi
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+#from modi.forms import modiForm
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def home(request):
+    return render(request, 'template/index.html')
